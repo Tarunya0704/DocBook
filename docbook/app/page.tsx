533e3965e3021ed7ac218063ad3c1cbc943a4c06
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2024-07-11 04:37:13 +0530

    setup completed

--- docbook/app/page.tsx
+++ docbook/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
-    <h1 className="text-3xl underline">hellllo</h1>
+    <h1 className=" text-white text-3xl underline">hellllo</h1>
+    <Button>click me</Button>
    </div>
   );
 }
+
