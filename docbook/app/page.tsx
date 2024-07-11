33cf1c8c5498454dee8544ab4e968cd33663155c
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2024-07-11 08:46:55 +0530

    workinG ON Homepage

--- docbook/app/page.tsx
+++ docbook/app/page.tsx
@@ -1,11 +1,41 @@
-import { Button } from "@/components/ui/button";
+
+import Patientform from "@/components/forms/Patientform";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <div>
-    <h1 className=" text-white text-3xl underline">hellllo</h1>
-    <Button>click me</Button>
+   <div className="flex h-screen max-h-screen ">
+    <section className="remove-scrollbar my-auto container">
+      <div className="sub-container max-w-[496px]">
+        <Image
+        src='/assets/icons/logo-full.svg'
+        height={1000}
+        width={1000}
+         alt="logo"
+         className="mb-12 h-10 w-fit" />
+         <Patientform/>
+         <div className="text-14-regular flex mt-20 justify-between">
+         <p className="justify-items-end text-dark-600 xl:text-left">
+          © 2024 DocBook
+         </p>
+         <Link href='/?admin=true' className="text-green-500">
+         Admin
+
+         </Link>
+         </div>
+
+      </div>
+
+    </section>
+<Image 
+src='/assets/images/onboarding-img.png'
+height={1000}
+width={1000}
+alt=''
+className="side-img max-w-[50%]"/>
+
+
    </div>
   );
 }
---
+++ docbook/components/forms/Patientform.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Patientform = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Patientform
+
